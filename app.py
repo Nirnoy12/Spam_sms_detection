f0fd3828cdc44bb9a9df57532f587002db183433
Nirnoy Chatterjee <nirnoychatterjee2004@gmail.com>
2025-06-27 12:40:38 +0530

    Changed location of nltk storage directory

--- app.py
+++ app.py
@@ -5,8 +5,15 @@ from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 
-nltk.download('punkt')
-nltk.download('stopwords')
+# Set download directory inside Streamlit's persistent storage
+nltk_data_dir = os.path.join(os.getcwd(), "nltk_data")
+os.makedirs(nltk_data_dir, exist_ok=True)
+
+nltk.download("punkt", download_dir=nltk_data_dir)
+nltk.download("stopwords", download_dir=nltk_data_dir)
+
+# Tell NLTK to use this directory
+nltk.data.path.append(nltk_data_dir)
 
 ps = PorterStemmer()
 
